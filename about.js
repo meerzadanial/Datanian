37aa79c2391fae201a9a299c3773d0e3449ccfe8
Danial <meerzadanial@gmail.com>
2023-09-26 12:26:56 +0800

    feat(complete-web-dev):modify contents

--- about.js
+++ about.js
@@ -15,3 +15,29 @@ const observer = new IntersectionObserver((entries)=>{
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el => observer.observe(el)));
     
+
+  $(document).ready(function () {
+    $(".managetree").owlCarousel({
+      loop: true,
+      margin: 30,
+      nav: true,
+      dots: true,
+      center: true,
+      navText: [
+        
+      ],
+      responsive: {
+        0: {
+          items: 1
+        },
+        600: {
+          items: 1
+        },
+        1000: {
+          items: 3
+        }
+      }
+    });
+  });
+  
+
